0dc48e3b8df8d8ee2cc1bad8a7a93cfa9c5072b8
jecano <jecano@personalsoft.com>
2022-07-22 16:55:31 -0500

    Se implementa la lógica necesaria para calcular la cantidad de decimales del número PI

--- src/main/java/com/pesonalsoft/pirandom/backend/apirest/services/PiServiceImpl.java
+++ src/main/java/com/pesonalsoft/pirandom/backend/apirest/services/PiServiceImpl.java
@@ -1,5 +1,8 @@
 package com.pesonalsoft.pirandom.backend.apirest.services;
 
+import java.text.DecimalFormat;
+import java.util.Random;
+
 import org.springframework.stereotype.Service;
 import com.pesonalsoft.pirandom.backend.apirest.models.PiResponse;
 
@@ -15,11 +18,29 @@ public class PiServiceImpl implements IPiService {
 		
 		PiResponse piResponse = new PiResponse();
 		
+		int random = obtenerNumeroRandom(input_number);
+		String piCalc = obtenerValorPi(random);
+		
 		piResponse.setParam(input_number);
-		piResponse.setRandom(9);
-		piResponse.setPiCalc("3.1416");
+		piResponse.setRandom(random);
+		piResponse.setPiCalc(piCalc);
 		
 		return piResponse;
 	}
+
+	private int obtenerNumeroRandom(int input_number) {
+		int rangoInicial = input_number/2;
+		int rangoFinal = input_number+1;
+		
+		Random random = new Random();
+		
+		return rangoInicial + random.nextInt((rangoFinal - rangoInicial) + 1); 
+	}
 	
+	private String obtenerValorPi(int random) {
+		DecimalFormat decFormat = new DecimalFormat();
+		decFormat.setMaximumFractionDigits(random);
+		
+		return decFormat.format(Math.PI);
+	}	
 }

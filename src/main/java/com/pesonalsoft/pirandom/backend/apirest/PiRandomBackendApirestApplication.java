b785baf650da2921ea921e88d21a423753efe86a
Jhon Cano <jhonalvarez.2011@gmail.com>
2022-07-22 10:29:48 -0500

    Aquí inicia el proyecto api rest build para MELI

---
+++ src/main/java/com/pesonalsoft/pirandom/backend/apirest/PiRandomBackendApirestApplication.java
@@ -0,0 +1,13 @@
+package com.pesonalsoft.pirandom.backend.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiRandomBackendApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiRandomBackendApirestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pesonalsoft/pirandom/backend/apirest/PiRandomBackendApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesonalsoft.pirandom.backend.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiRandomBackendApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
